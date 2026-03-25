23b20a282af00ed0951ad0d4a6dbdca5ef823cc7
원이 <hawon9781@gmail.com>
2026-03-25 17:11:42 +0900

    프로젝트 생성 ✨

---
+++ KDH-Watch/KDH-Watch Watch App/KDH_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDH_WatchApp.swift
+//  KDH-Watch Watch App
+//
+//  Created by hawon on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct KDH_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
